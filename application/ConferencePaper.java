a99ec37f44975f2abc1331259af877d9ed8f894e
Mccullahz <zylardmccullah@gmail.com>
2024-04-15 14:41:49 -0400

    I was being silly didn't look at Publication very well before sedning commit

--- application/ConferencePaper.java
+++ application/ConferencePaper.java
@@ -1,6 +1,6 @@
 /*
 *
-* @author: Zylar McCullah
+* @author: Zylar :)
 *
 */
 package application;
@@ -9,15 +9,13 @@ public class ConferencePaper extends Publication {
     private AuthorList authorList;
     private String conferenceName;
     private Location location;
-    private String period; // period should need broken down more i believe
+    private String period; // period should need broken down more I believe
 
-    public ConferencePaper(String title, int publicationYear, AuthorList authorList, String conferenceName, Location location, String period) {
-        super(title, publicationYear);
+    public ConferencePaper(String title, String pub_year, AuthorList authorList, String conferenceName, Location location, String period) {
+        super(title, pub_year);
         this.authorList = authorList;
         this.conferenceName = conferenceName;
         this.location = location;
         this.period = period;
     }
-
-    // Getters and setters
 }
