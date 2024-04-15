b1b4a9001ec9b6cc5bfcec01bca7e52906455203
Mccullahz <zylardmccullah@gmail.com>
2024-04-15 14:31:38 -0400

    ConferencePaper file added

--- application/ConferencePaper.java
+++ application/ConferencePaper.java
@@ -1,5 +1,23 @@
-package cs.CS3700.LMS.application;
+/*
+*
+* @author: Zylar McCullah
+*
+*/
+package application;
 
-public class ConferencePaper {
-    
+public class ConferencePaper extends Publication {
+    private AuthorList authorList;
+    private String conferenceName;
+    private Location location;
+    private String period; // period should need broken down more i believe
+
+    public ConferencePaper(String title, int publicationYear, AuthorList authorList, String conferenceName, Location location, String period) {
+        super(title, publicationYear);
+        this.authorList = authorList;
+        this.conferenceName = conferenceName;
+        this.location = location;
+        this.period = period;
+    }
+
+    // Getters and setters
 }
