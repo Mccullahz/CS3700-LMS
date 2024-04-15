c23b24f0fefb1a45679ddedd41d6e5e9dd519271
Mccullahz <zylardmccullah@gmail.com>
2024-04-15 14:35:37 -0400

    AuthorList(more like author array >:( ), Location classes added.

---
+++ application/AuthorList.java
@@ -0,0 +1,22 @@
+/*
+*
+* @author: Zylar :)
+*
+*/
+package application;
+
+public class AuthorList {
+    private String[] authors;
+
+    public AuthorList(String[] authors) {
+        this.authors = authors;
+    }
+    
+    public String[] getAuthors() {
+        return authors;
+    }
+    
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+}
---
+++ application/Location.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Location {
+    private String locationName;
+
+    public Location(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+}
