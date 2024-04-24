657a264848c67495a3a8225a937c1445689b7c63
LaniKN <nohealani2022@gmail.com>
2024-04-24 14:25:33 -0400

    research inheritance fixed :)

--- application/ResearchReport.java
+++ application/ResearchReport.java
@@ -3,14 +3,11 @@
 package application;
 
 public class ResearchReport extends Publication {
-    public Author[] author_list;
 
     public ResearchReport() {
         super();
-        author_list = new Author[0];
     }
     public ResearchReport(String t, String py, int an) {
-        super(t, py);
-        author_list = new Author[an];
+        super(t, py, an);
     }
 }
