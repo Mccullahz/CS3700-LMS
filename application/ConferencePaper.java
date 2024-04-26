6b31c98af76aa2b0dd50b8a957c17d74a5d60514
Mccullahz <zylardmccullah@gmail.com>
2024-04-26 03:13:12 -0400

    db aside, this file should be ready to roll now, i have done ZERO testing for it though :)

--- application/ConferencePaper.java
+++ application/ConferencePaper.java
@@ -7,12 +7,13 @@ public class ConferencePaper extends Publication {
     public AuthorList authorList;
     public String conferenceName;
     public Location location;
-    
+    public Period period;
 
-    public ConferencePaper(String title, String pub_year, AuthorList authorList, String conferenceName, Location location, String period) {
+    public ConferencePaper(String title, String pub_year, AuthorList authorList, String conferenceName, Location location, Period period) {
         super(title, pub_year);
         this.authorList = authorList;
         this.conferenceName = conferenceName;
         this.location = location;
+        this.period = period;
     }
 }
