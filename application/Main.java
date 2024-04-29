0860817994cb719f1e3b94cdbed6ce1d3dfe98ac
Mccullahz <zylardmccullah@gmail.com>
2024-04-28 21:31:47 -0400

    who needs the extra stuff in main, thats for nerds

--- application/Main.java
+++ application/Main.java
@@ -1,7 +1,8 @@
 package application;
-// This Main Class is the foundation of the application. This is what will be used to create our GUI. Options for Java GUI seem to be:
-// Java Swing or JavaFX 
 
 public class Main{
+    public static void main(String[] args) {
+    
+    }
     
 }
