483eb416bdca22ebe894c0e8dca9b53da87af2ff
Mccullahz <zylardmccullah@gmail.com>
2024-04-28 21:29:45 -0400

    needed to add author class to branch

---
+++ application/Author.java
@@ -0,0 +1,26 @@
+//Written by ALiilani McGuire
+
+package application;
+
+public class Author {
+    private String first_name, last_name;
+
+    public Author() {
+        first_name = "John";
+        last_name = "Doe";
+    }
+
+    public Author(String fn, String ln) {
+        first_name = fn;
+        last_name = ln;
+    }
+
+    public void setFirstName(String fn) { first_name = fn; }
+    public void setLastName(String ln) { last_name = ln; }
+    public String getFirstName() { return first_name; }
+    public String getLastName() { return last_name; }
+
+    public String getString(){
+        return "\n\tFirst Name: " + first_name + "\n\tLast Name: " + last_name;
+    }
+}
