aaf0c8a5c53b9fdae8d88c99171956285cc3b42b
Cherico <Welchcherico@gmail.com>
2024-05-03 13:13:39 -0400

    Update Thesis.java

--- application/Thesis.java
+++ application/Thesis.java
@@ -1,5 +1,58 @@
 package application;
-
-public class Thesis extends Published{
+//Cherico Welch
+public class Thesis extends Published {
     
+    private Department department;
+    private CommitteeMember[] committeeMember;
+    private Chapters chapters;
+    private Figures figures;
+    
+    public Thesis(){
+        Department = new Department();
+        committeeMember = CommitteeMember();
+        chapters = new Chapters();
+        figures = new Figures();
+    }
+
+    public Dissertation(String title, String publicationYear, String departmentName, DeparmentName, Department department, committeeMember[] committeeMembers, Chapters chapters, Figures figures){
+        super(title, publicationYear, 1);
+        this.department = department;
+        this.committeeMembers = committeeMembers;
+        this.chapters = chapters;
+        this.figures = figures;
+    }
+
+    public Department getDepartment(){
+        return department;
+    }
+
+    public void setDepartment(Department department){
+        return department;
+    }
+
+    public CommitteeMember [] getCommitteeMembers(){
+        return CommitteeMembers;
+    }
+
+    public void setCommitteeMembers(CommitteeMember[] committeeMembers) {
+        this.committeeMembers = committeeMembers;
+    }
+
+    public Chapters getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(Chapters chapter) {
+        this.chapters = chapters;
+    }
+
+    public Figures getFigures() {
+        return figures;
+    }
+
+    public void setFigures(Figures figures) {
+        this.figures = figures;
+    }
 }
+   
+
